4731f377a907673e7ff583beabed7df840f1046d
Pavel <pavel.pravdin@gmail.com>
2026-01-22 00:37:18 +0700

    chore: Update AICHE Desktop to v1.0.21

--- Casks/aiche-desktop.rb
+++ Casks/aiche-desktop.rb
@@ -1,15 +1,15 @@
 cask "aiche-desktop" do
-  version "1.0.20"
+  version "1.0.21"
 
   # Architecture-specific downloads
   on_arm do
-    sha256 "c8d76c41ade31b157049e8353e5ce1b190cfc86ac9730deaa493ab4dd6291cca"
+    sha256 "70d226d84395d765827d00faf84883a207c13a64f475bf3dd921b6f4e67c7b0f"
     url "https://github.com/AICHE-app/mac-releases/releases/download/v#{version}/AICHE-Installer-#{version}-ARM64.dmg",
         verified: "github.com/AICHE-app/mac-releases/"
   end
 
   on_intel do
-    sha256 "2b55f87a9f8bf1f42899c86f153160cfe818771161cd19ff01d6c6c70d05ba14"
+    sha256 "84f82c1826350622574991025e94fa9bd68afbc107d47954fa068530e2acd381"
     url "https://github.com/AICHE-app/mac-releases/releases/download/v#{version}/AICHE-Installer-#{version}-Intel.dmg",
         verified: "github.com/AICHE-app/mac-releases/"
   end
